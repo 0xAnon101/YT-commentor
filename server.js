ea610108bcb11eea7e1d2d761f936e7c4e605e2a
Raju Gautam <Rajugautam45@gmail.com>
2018-08-30 22:03:10 +0800

    added static path for client js

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const crypto = require('crypto');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use('/assets',express.static(path.join(__dirname, '/public/stylesheets')));
+app.use('/assets/css',express.static(path.join(__dirname, '/public/stylesheets')));
+app.use('/assets/js',express.static(path.join(__dirname+'/public/javascript')));
 app.engine('.hbs',exphbs({defaultLayout:'layout',extname:'.hbs'}));
 app.set('view engine','.hbs');
 
