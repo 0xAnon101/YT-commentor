f74733b194c92dc5ac81b0b09ebba6238a3b47b0
Raju Gautam <Rajugautam45@gmail.com>
2018-08-30 22:00:54 +0800

    added client side js

---
+++ public/javascript/app.js
@@ -0,0 +1,29 @@
+const button= document.querySelector('#button2');
+const input2 =  document.querySelector('#input-data1');
+
+
+const sendData = (event) => {
+    event.preventDefault();
+    const filterUrl =  input2.value.indexOf('&') != -1 ? input2.value.slice(0,input2.value.indexOf('&')) : input2.value; 
+    const url = new URL(filterUrl).searchParams.get("v");
+    fetch('/randomCommentView', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({urlname: url.toString()})
+      })
+      .then(function(res){ console.log(res) })
+      .catch(function(error){ console.log(error)});
+}
+
+button.addEventListener('click', function(event){
+    sendData(event);
+});
+
+input2.addEventListener('keypress', function (event) {
+    
+    var key =  event.charCode || event.keyCode;
+    key == 13 ? sendData(event) : null;
+ 
+});
