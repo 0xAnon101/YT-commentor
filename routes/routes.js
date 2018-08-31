57bf12038a3ca8efb7ecce2cc5334337a53da58c
Raju Gautam <Rajugautam45@gmail.com>
2018-09-01 02:19:04 +0800

    added post and handled parameters

--- routes/routes.js
+++ routes/routes.js
@@ -2,24 +2,27 @@ let DATA_URL = "";
 let QUERY_STRING = "";
 let x = "";
 let valid = true;
+let DATA_COMMENT = [];
 
-const Output = require('../quickstart').output;
+const Output = require('../channelDetails').output;
+const Comments = require('../randomComments').comment;
 
 module.exports = (app, crypto) => {
 
-    app.get('/', (req, res) => {
-        res.render('container/index', {
-            title: 'Hello User!',
-            comtent: 'Welcome to youtube Comment viewer'
+    app.route('/')
+        .get((req, res) => {
+            res.render('container/index', {
+                title: 'Hello User!',
+                content: 'Welcome to youtube Comment viewer'
+            });
         });
-    });
 
     app.route('/commentController')
         .post((req, res) => {
             DATA_URL = req.body.data_url;
 
             try {
-                QUERY_STRING = new URL(req.body.data_url).pathname.match(/^\/channel\/(.+)$/)[1];
+                QUERY_STRING = new URL(DATA_URL).pathname.match(/^\/channel\/(.+)$/)[1];
             } catch (err) {
                 valid = false;
                 if (err) res.status(404).render('container/index', {
@@ -28,7 +31,7 @@ module.exports = (app, crypto) => {
             }
             console.log(valid)
             if (valid) {
-                crypto. pbkdf2(QUERY_STRING, '3745e48...08d59ae', 100000, 64, 'sha512', (err, derivedKey) => {
+                crypto.pbkdf2(QUERY_STRING, '3745e48...08d59ae', 100000, 64, 'sha512', (err, derivedKey) => {
                     if (err) throw err;
                     x = derivedKey.toString('hex');
                 });
@@ -47,28 +50,37 @@ module.exports = (app, crypto) => {
     app.get('/result/:value/:secret', (req, res) => {
         Output(QUERY_STRING)
             .then((data) => {
-
-                console.log('This channel\'s ID is %s. Its title is \'%s\', and ' +
-                    'it has %s views.',
-                    data[0].id,
-                    data[0].snippet.title,
-                    data[0].statistics.viewCount);
+                res.render('container/result', {
+                    Id: data[0].id,
+                    Title: data[0].snippet.title,
+                    ViewCount: data[0].statistics.viewCount
+                });
             }).catch((err) => {
                 if (err) res.status(404).render('container/index', {
                     notFound: `Your request couldn't be completed ERR: ${err}`
                 })
             })
-        res.render('container/result', {
-            title: 'Hello User!',
-            comtent: 'Welcome to youtube Comment viewer',
-            URL: DATA_URL,
-            QUERY: QUERY_STRING
+
+    });
+
+    app.route('/randomCommentView')
+        .post((req, res) => {
+            Comments(req.body.urlname)
+                .then((data) => {
+                    DATA_COMMENT = data;
+                    console.log(DATA_COMMENT);
+                }).catch(err => {
+                    if (err) res.status(404).render('container/index', {
+                        notFound: `Your request couldn't be completed ERR: ${err}`
+                    })
+                })
+            res.redirect('/getComment');
+        })
+
+    app.get('/getComment', (req, res) => {
+        res.render('container/comment', {
+            Comment:DATA_COMMENT
         });
-    });
+    })
 
-    app.post('/randomCommentView',(req,res)=>{
-        console.log(req.body);
-    });
 }
-
-module.exports.QUERY_STRING = QUERY_STRING;
